e8b6a1eeb4b4c4296edc04a2d46082e81272b9e9
Maria Garcia <100429121@alumnos.uc3m.es>
2023-04-05 18:48:16 +0200

    main, neural network(logistic regresision)

---
+++ NeuralNetwork.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import ModelCheckpoint
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import set_random_seed
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+from matplotlib import pyplot as plt
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+
+
+class NeuralNetworkLR:
+    def __init__(self, epochs, lr, train_data, test_data, validate_data, neurons, model_path):
+        """
+        epochs = int, número de "rondas" de entrenamiento
+        lr = float, tasa de aprendizaje
+        train_data = dataset, datos de entrenamiento
+        validate_data = dataset, datos de validación
+        test_data = dataset, datos de test
+        neurons = lista python, neuronas de las capas ocultas
+        model_path = path para guardar el modelo
+        
+        """
+        self.epochs = epochs
+        self.lr = lr
+        self.train_data = train_data
+        self.validate_data = validate_data
+        self.test_data = test_data
+        self.neurons = neurons
+        self.model = None
+        self.X_train = None
+        self.Y_train = None
+        self.X_validate = None
+        self.Y_validate = None
+        self.X_test = None
+        self.Y_test = None
+        self.model_path = model_path
+            
+    def create_network(self):
+        self.model = Sequential()
+        #capa de entrada
+        self.model.add(Dense(self.train_data.shape[1], activation = 'relu'))
+        #capas ocultas
+        for layer in range (len(self.neurons)-1):
+            self.model.add(Dense(self.neurons[layer], activation = 'relu'))
+        #capa de salida
+        self.model.add(Dense(self.neurons[-1]), activation = 'softmax')
+        
+    """def checkpoint(self):
+       checkpoint = ModelCheckpoint(self.model_path + '/model', monitor='val_loss', verbose=1, save_best_only=True)
+       return checkpoint"""
+      
+    def split_datasets(self):
+        self.X_train = None
+        self.Y_train = None
+        self.X_validate = None
+        self.Y_validate = None
+        self.X_test = None
+        self.Y_test = None
+    
+    def train(self):
+        self.model.compile(optimizer = tf.keras.optimizers.SDG(learning_rate = self.lr, momentum = 0), loss= 'mean_squared_error', metrics=['accuracy', 'mse'])
+        historico = self.model.fit(self.X_train, self.Y_train, epochs = self.epochs, batch_size = 16, validation_data= (self.X_validate, self.Y_validate), shuffle = False, validation_freq = 1) #'''callbacks = [self.checkpoint()],''' 
+        epochs_stop = np.where(historico.history['val_loss'] == np.min(historico.history['val_loss']))
+        final_epoch=epochs_stop[0][0]
+        metrics = {'accuracy': historico.history['accuracy'][final_epoch -1],
+                   'loss': historico.history['loss'][final_epoch -1],
+                   'val_accuracy': historico.history['val_accuracy'][final_epoch -1],
+                   'val_loss': historico.history['val_loss'][final_epoch -1]}
+        return metrics
---
+++ main.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import ModelCheckpoint
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.utils import set_random_seed
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+from matplotlib import pyplot as plt
+from NeuralNetwork import *
+import os
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+TRAINSET_PATH = "./data/train_set"
+VALIDATESET_PATH = "./data/train_set"
+TESTSET_PATH = "./data/test_sest"
+MODEL_PATH = "./models/model_1"
+
+os.mkdir(MODEL_PATH)
+train_set = pd.read_csv(TRAINSET_PATH)
+train_set = train_set.to_numpy(dtype = 'float64')
+validate_set = pd.read_csv(VALIDATESET_PATH)
+validate_set = validate_set.to_numpy(dtype = 'float64')
+test_set = pd.read_csv(TESTSET_PATH)
+test_set = test_set.to_numpy(dtype = 'float64')
+
+neural_network = NeuralNetworkLR(300, 0.1, train_set, test_set, validate_set, [10,10], MODEL_PATH)
+neural_network.create_network()
+neural_network.split_datasets()
+results = neural_network.train()
+
+file = open(MODEL_PATH + "/results.txt", "w")
+file.write(results)
+file.close()
+
+plt.plot(results['accuracy'])
+plt.plot(results['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.savefig(MODEL_PATH + '/model_accuracy.jpg')
+plt.show()
+
+plt.plot(results['loss'])
+plt.plot(results['val_loss'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.savefig(MODEL_PATH+'/model_loss.jpg')
+plt.show()
