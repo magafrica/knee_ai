a7d0d868f25ce90cd07bf9e4b3afebecf37c6d08
Maria Garcia <100429121@alumnos.uc3m.es>
2023-05-15 16:39:59 +0200

    train and test for 0.8

--- data/merge_files.py
+++ data/merge_files.py
@@ -2,22 +2,23 @@ import csv
 import pandas as pd
 # Nombre de los archivos CSV
 DATASET_PATH = "./data/datasets/"
-sinteticos = DATASET_PATH + 'sinteticos_mayoria_7.csv'  # CSV con 5000 filas
-datos_procesados= DATASET_PATH + 'datos_procesados_mayoria_7.csv'  # CSV con 167 filas
+sinteticos = DATASET_PATH + 'sinteticos_mayoria_8.csv'  # CSV con 5000 filas
+datos_procesados= DATASET_PATH + 'datos_procesados_mayoria_8.csv'  # CSV con 167 filas
 
 # Nombre de los archivos de salida
-train_csv = DATASET_PATH + 'train_mayoria_7.csv'    # CSV con 5000 filas
-test_csv = DATASET_PATH + 'test_mayoria_7.csv'      # CSV con 67 filas
+train_csv = DATASET_PATH + 'train_mayoria_8.csv'    # CSV con 5000 filas
+test_csv = DATASET_PATH + 'test_mayoria_8.csv'      # CSV con 67 filas
 
 df = pd.read_csv(sinteticos, sep=';')
 print(df.head())
-df.drop(columns=['Unnamed: 0','Unnamed: 0.1','Unnamed: 0.1.1','Unnamed: 0.1.1.1','Unnamed: 0.1.1.1.1'], inplace=True)
+df.drop(columns=['Unnamed: 0','Unnamed: 0.1','Unnamed: 0.1.1'], inplace=True)
 df.to_csv(sinteticos, sep=';')
 df = pd.read_csv(datos_procesados, sep=';')
 print(df.head())
 df.drop(columns=['Unnamed: 0','Unnamed: 0.1'], inplace=True)
 df.to_csv(datos_procesados, sep=';')
 # Leer el contenido del primer archivo CSV
+
 datos1 = []
 with open(sinteticos, 'r') as archivo:
     lector = csv.reader(archivo)
